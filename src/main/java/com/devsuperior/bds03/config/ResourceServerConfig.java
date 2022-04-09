1f3517fcdda3592b0360d4684f7b050be803d485
unknown <erick.oliveira9607@gmail.com>
2022-04-09 20:49:23 -0300

    satisfying tests 2

--- src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
@@ -18,7 +18,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
-    private static final String[] OPERATOR_OR_ADMIN = {"/employees/**", "/departments/**", "/users/**"};
+    private static final String[] OPERATOR_GET = {"/employees/**", "/departments/**"};
 
     private static final String[] ADMIN = {"/users/**"};
 
@@ -41,9 +41,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
-                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).hasAnyRole("ADMIN", "OPERATOR")
-                .antMatchers(OPERATOR_OR_ADMIN).hasRole("ADMIN")
-                .antMatchers(ADMIN).hasRole("ADMIN")
-                .anyRequest().authenticated();
+                .antMatchers(HttpMethod.GET, OPERATOR_GET).hasAnyRole("ADMIN", "OPERATOR")
+                .anyRequest().hasAnyRole("ADMIN");
     }
 }
